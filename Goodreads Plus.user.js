b928497f7d63406a0270b6291520aa0759ba3af2
Andrew Innes <andrew.c12@gmail.com>
2024-02-15 05:18:54 +0800

    0.2
    Author	Slengpung
    Version	0.2
    Created	2016-11-09
    Updated	2017-03-11
    Applies to	goodreads.com

--- Goodreads Plus.user.js
+++ Goodreads Plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Goodreads Plus
 // @namespace    https://greasyfork.org/en/users/78880
-// @version      0.1
+// @version      0.2
 // @description  Add "Search MAM" button to Goodreads
 // @author       Slengpung
 // @include      https://www.goodreads.com/*
@@ -9,22 +9,44 @@
 // @downloadURL none
 // ==/UserScript==
 
-console.log("[M+] Tweaking Goodreads...");
+console.log("[G+] Tweaking Goodreads...");
 
-// Grab book title (and only title)
-var bookTitle = document.getElementById("bookTitle").innerHTML.trim().split('<', 1);
-console.log("Book title: " + bookTitle);
-var mamSearchUrl = "https://www.myanonamouse.net/tor/browse.php?tor[text]=" + bookTitle;
+var page = window.location.pathname.split('/')[1];
 
-// Add 'Search MAM' button
-var buttonBar = document.getElementById("buyButtonContainer");
-if (buttonBar === null || buttonBar == "null") {
-	buttonBar = document.getElementById("asyncBuyButtonContainer");
+if (page === 'book') {
+	var bookTitle = getBookTitle(document.getElementById("bookTitle"));
+	var mamSearchUrl = "https://www.myanonamouse.net/tor/browse.php?tor[text]=" + bookTitle;
+
+	// Add 'Search MAM' button
+	var buttonBar = document.getElementById("buyButtonContainer");
+	if (buttonBar === null || buttonBar == "null") {
+		buttonBar = document.getElementById("asyncBuyButtonContainer");
+	}
+	var buttonUl = buttonBar.getElementsByTagName("ul");
+	var mamButton = document.createElement("li");
+	mamButton.innerHTML = '<a id="mamLink" href="' + mamSearchUrl + '" target="_blank" class="buttonBar">Search MAM</a>';
+	mamButton.className = "Button";
+	buttonUl[0].appendChild(mamButton);
+	console.log("[G+] 'Search MAM' button added!");
+} else if (page === 'review') {
+	var bookList = document.querySelectorAll('#booksBody .title div a');
+	// Loop over all the books
+	for (var i = 0; i < bookList.length; i++) {
+		var mamSearchUrl = "https://www.myanonamouse.net/tor/browse.php?tor[text]=" + getBookTitle(bookList[i]);
+		// Add 'Search MAM' button
+		var newLink = document.createElement('a');
+		var linkText = document.createTextNode('[Search MAM]');
+		newLink.appendChild(linkText);
+		newLink.setAttribute('href', mamSearchUrl);
+		newLink.setAttribute('style', 'color:#b3b3b3;font-style:italic');
+		bookList[i].parentNode.parentNode.appendChild(newLink);
+	}
+	console.log("[G+] 'Search MAM' buttons added!");
 }
-var buttonUl = buttonBar.getElementsByTagName("ul");
-var mamButton = document.createElement("li");
-mamButton.innerHTML = '<a id="mamLink" href="' + mamSearchUrl + '" target="_blank" class="buttonBar">Search MAM</a>';
-mamButton.className = "Button";
-buttonUl[0].appendChild(mamButton);
 
-console.log("[M+] 'Search MAM' button added!");
+// Grab book title (and only title) from the element
+function getBookTitle(el) {
+	var bookTitle = el.innerHTML.trim().split('<', 1) + '';
+	console.log("Book title: " + bookTitle.trim());
+	return bookTitle.trim();
+}
