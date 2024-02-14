b92807b330d31f11ee4b0c880f795a7d65b3ece5
Andrew Innes <andrew.c12@gmail.com>
2024-02-15 06:51:41 +0800

    0.3.9
    Author	Slengpung
    Version	0.3.9
    Created	2016-11-09
    Updated	2022-08-21
    License	MIT
    Applies to	goodreads.com

--- Goodreads Plus.user.js
+++ Goodreads Plus.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Goodreads Plus DEV
+// @name         Goodreads Plus
 // @namespace    https://greasyfork.org/en/users/78880
-// @version      0.3.8
+// @version      0.3.9
 // @description  Add "Search MAM" button to Goodreads
 // @author       Slengpung
 // @include      https://www.goodreads.com/*
@@ -14,33 +14,33 @@ console.log("[G+] Tweaking Goodreads...");
 
 var page = window.location.pathname.split('/')[1];
 
-if (page === 'book') {
-	console.log("[G+] We got a book URL");
+if(page === 'book'){
+    console.log("[G+] We got a book URL");
 	var bookTitle = document.getElementsByClassName("Text__title1")[0].innerHTML;
 	console.log("[G+] Book title: " + bookTitle);
 	var mamSearchUrl = "https://www.myanonamouse.net/tor/browse.php?tor[text]=" + bookTitle;
 
 	// Add 'Search MAM' button
 	var buttonBar = document.getElementsByClassName("BookActions")[0];
-	var mamButton = document.getElementsByClassName("BookActions__button")[0].cloneNode(true);
-	mamButton.innerHTML = '<div class="BookActions__button"><a href="' + mamSearchUrl + '">' +
-		'<div class="Button__container Button__container--block">' +
-		'<button type="button" class="Button Button--secondary Button--medium Button--block">' +
-		'<span class="Button__labelItem">Search MAM</span></button></div></a></div>';
-
+	var mamButton  = document.getElementsByClassName("BookActions__button")[0].cloneNode(true);
+	mamButton.innerHTML = '<div class="BookActions__button"><a href="' + mamSearchUrl + '">' + 
+	    '<div class="Button__container Button__container--block">' + 
+	    '<button type="button" class="Button Button--secondary Button--medium Button--block">' + 
+	    '<span class="Button__labelItem">Search MAM</span></button></div></a></div>';
+	
 	buttonBar.appendChild(mamButton)
 	console.log("[G+] 'Search MAM' button added!");
-} else if (page === 'review') {
+}else if(page === 'review'){
 	var bookList = document.querySelectorAll('#booksBody .title div a');
 	// Loop over all the books
-	for (var i = 0; i < bookList.length; i++) {
+	for(var i=0; i<bookList.length; i++){
 		var mamSearchUrl = "https://www.myanonamouse.net/tor/browse.php?tor[text]=" + getBookTitle(bookList[i]);
 		// Add 'Search MAM' button
 		var newLink = document.createElement('a');
 		var linkText = document.createTextNode('[Search MAM]');
 		newLink.appendChild(linkText);
-		newLink.setAttribute('href', mamSearchUrl);
-		newLink.setAttribute('style', 'color:#b3b3b3;font-style:italic');
+		newLink.setAttribute('href',mamSearchUrl);
+		newLink.setAttribute('style','color:#b3b3b3;font-style:italic');
 		bookList[i].parentNode.parentNode.appendChild(newLink);
 	}
 	console.log("[G+] 'Search MAM' buttons added!");
